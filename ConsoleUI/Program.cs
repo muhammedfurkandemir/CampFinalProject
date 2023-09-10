e6687cedd2ff519ef7cae0199a5fbb1b69df39ae
Furkan Demir <furkan.vty0@gmail.com>
2023-09-10 19:44:27 +0300

    Add WebAPI and ProductController

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,7 +11,9 @@ namespace ConsoleUI
             //Data Transformation Object 
             //CategoryTest();
             //IoC
-            ProductTest();
+            //ProductTest();
+
+            
 
         }
 
@@ -28,10 +30,20 @@ namespace ConsoleUI
         {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetProductDetails())
+            var result = productManager.GetProductDetails();
+            if (result.Success)
             {
-                Console.WriteLine(product.ProductName +" / "+product.CategoryName);
+                foreach (var product in result.Data)
+                {
+                    Console.WriteLine(product.ProductName + " / " + product.CategoryName);
+                }
             }
+            else
+            {
+                Console.WriteLine(result.Message);
+            }
+
+            
         }
     }
 }
---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,65 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Concrete.EntityFreamwork;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        //aşağıdaki gibi bir constroctır ile bağladığımızda losely coupled : zayıf bağlılık denir.
+        //bir sınıfa veya bir şeye bağımlılık olacak sa asla sınıf gibi somut olanlarla değil soyut varlıklarla 
+        //interface gibi bağlanmalıdır.
+        //IoC Container -- Inversion of Control
+        //IProductService _productService;
+
+        //public ProductsController(IProductService productService)
+        //{
+        //    _productService = productService;
+        //}
+        [HttpGet("getall")]
+        public IActionResult GetAll()
+        {
+            //eğer burda direk olarak product service new leyerek çağırsaydım bir bağımlılık olacaktı.
+            //bu bağımlılığa dependency chain : bağımlılık zinciri denir
+            //swagger : hazır dökümantasyon imkanı sağlar.
+
+            //var result= _productService.GetAll();
+
+            IProductService productService = new ProductManager(new EfProductDal());
+            var result=productService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+        [HttpGet("getbyid")]
+        public IActionResult GetById(int id)
+        {
+            IProductService productService = new ProductManager(new EfProductDal());
+            var result = productService.GetById(id);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpPost("add")]
+        public IActionResult Add(Product product)
+        {
+            IProductService productService = new ProductManager(new EfProductDal());
+            var result = productService.Add(product);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result); 
+        }
+    }
+}
